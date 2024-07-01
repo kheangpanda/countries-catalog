6988201f2254ad6874c468e21b7dc097052b05a2
kheangpanda <kheangch0@gmail.com>
2024-07-01 22:34:07 +0700

    refactor: :art: clean and change title and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLayout from "./components/Layout";
 
 function App() {
+  useEffect(() => {
+    document.title = "Country Catalogs";
+  }, []);
   return (
     <div className="App">
       <AppLayout />
