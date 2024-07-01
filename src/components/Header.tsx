0fec2f8ed144a1a11b3a57788566597135b54bf9
kheangpanda <kheangch0@gmail.com>
2024-07-01 21:44:11 +0700

    refactor: :tada: init sort, pagination and search with fuzzy search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,105 +61,19 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const PrimarySearchAppBar = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
+interface HeaderSearch {
+  search: string;
+  handleSearch: any;
+  handleClickSort: any;
+  sort: number;
+}
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
-
+const PrimarySearchAppBar = ({
+  search,
+  handleSearch,
+  handleClickSort,
+  sort,
+}: HeaderSearch) => {
   return (
     <Box sx={{ flexGrow: 1, mb: 5 }}>
       <AppBar position="static">
@@ -188,6 +102,8 @@ const PrimarySearchAppBar = () => {
             <StyledInputBase
               placeholder="Search…"
               inputProps={{ "aria-label": "search" }}
+              value={search}
+              onChange={(e) => handleSearch(e)}
             />
           </Search>
 
@@ -201,8 +117,12 @@ const PrimarySearchAppBar = () => {
               aria-label="show 4 new mails"
               color="inherit"
               sx={{ flexDirection: "column" }}
+              onClick={() => handleClickSort()}
             >
-              <Badge badgeContent={<ArrowDropUpIcon />} color="info">
+              <Badge
+                badgeContent={sort === 0 ? "-" : sort === 1 ? "A-Z" : "Z-A"}
+                color="info"
+              >
                 <FilterAltIcon />
               </Badge>
               <Typography
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,21 +6,63 @@ import PrimarySearchAppBar from "./Header";
 import { fetchGetCountries } from "../api";
 import { CountryInfo } from "../type";
 
+import Fuse from "fuse.js";
+
 const AppLayout = () => {
   const [data, setData] = useState<CountryInfo[]>([]);
+  const [mainData, setMainData] = useState<CountryInfo[]>([]);
   const [page, setPage] = React.useState(0);
   const [limit, setLimit] = React.useState(25);
   const [count, setCount] = useState<number>(0);
+  const [search, setSearch] = useState<string>("");
+  //1 = Asc, 2= Desc
+  const [sort, setSort] = useState<number>(1);
 
   useEffect(() => {
     const getCountries = async () => {
-      let data = await fetchGetCountries();
-      setCount(data.length);
-      data = data.slice(page * limit, limit + page * limit);
-      setData(data);
+      let getData = await fetchGetCountries();
+      setCount(getData.length);
+      setMainData(getData);
+
+      getData = getData.sort((a: CountryInfo, b: CountryInfo) => {
+        const nameA = a.name.official.toLowerCase();
+        const nameB = b.name.official.toLowerCase();
+        if (sort === 1) return nameA < nameB ? -1 : 1;
+        return nameA > nameB ? -1 : 1;
+      });
+
+      getData = getData.slice(page * limit, limit + page * limit);
+
+      setData(getData);
     };
     getCountries();
-  }, [page, limit]);
+  }, []);
+
+  useEffect(() => {
+    handleSearchAndSort();
+  }, [search, sort, page, limit]);
+
+  const handleSearchAndSort = () => {
+    const fuse = new Fuse(mainData, {
+      keys: ["name.official"],
+      threshold: 0.3,
+    });
+
+    let filtered = search
+      ? fuse.search(search).map((result) => result.item)
+      : mainData;
+
+    filtered = filtered.sort((a, b) => {
+      const nameA = a.name.official.toLowerCase();
+      const nameB = b.name.official.toLowerCase();
+      if (sort === 1) return nameA < nameB ? -1 : 1;
+      return nameA > nameB ? -1 : 1;
+    });
+
+    filtered = filtered.slice(page * limit, limit + page * limit);
+
+    setData(filtered);
+  };
 
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement> | null,
@@ -36,9 +78,24 @@ const AppLayout = () => {
     setPage(0);
   };
 
+  const handleSearch = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClickSort = () => {
+    setSort((preVal) => (preVal === 1 ? 2 : 1));
+  };
+
   return (
     <>
-      <PrimarySearchAppBar />
+      <PrimarySearchAppBar
+        handleSearch={handleSearch}
+        search={search}
+        handleClickSort={handleClickSort}
+        sort={sort}
+      />
       <ListCountries
         items={data}
         page={page}
