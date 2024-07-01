1269a5a4146276a22146aa60702521bade4d706b
kheangpanda <kheangch0@gmail.com>
2024-07-01 14:34:06 +0700

    style: :art: init style marterial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Button} from '@mui/material'
 function App() {
   return (
     <div className="App">
       <h1>Country Categories</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 }
